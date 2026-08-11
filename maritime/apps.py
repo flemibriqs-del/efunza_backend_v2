c06c0c61696e31f8ad4f61c52346e68c8d40eb36
flemibriqs-del <flemibriqs@gmail.com>
2026-08-11 10:06:08 +0300

    chore(maritime): add AppConfig

---
+++ maritime/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MaritimeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'maritime'
+    verbose_name = 'Maritime Academy'
