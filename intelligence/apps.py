ba09f384ccd7f8b487c4c2bc9f545ee2aac13ffb
flemibriqs-del <flemibriqs@gmail.com>
2026-08-12 18:45:06 +0300

    Change verbose name for Intelligence app

--- intelligence/apps.py
+++ intelligence/apps.py
@@ -1,7 +1,17 @@
+"""
+Intelligence App Configuration
+"""
+
 from django.apps import AppConfig
 
 
 class IntelligenceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'intelligence'
-    verbose_name = 'Efunza Intelligence'
+    verbose_name = 'Intelligence Engine'
+    
+    def ready(self):
+        """
+        Import signals when the app is ready.
+        """
+        import intelligence.signals
